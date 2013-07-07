0a7b5828a9e96110e389464674cb4c91e5a2eae2
Noah Moroze <noahm@moroze.com>
2013-07-07 04:28:18 +0000

    Added new page

--- web.js
+++ web.js
@@ -6,6 +6,10 @@ app.get('/', function(request, response) {
   response.send("I'm watching you 0.0");
 });
 
+app.get('/hi',function(request, response) {
+    response.send("Hi");
+});
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
