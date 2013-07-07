94ce109249b519bca4d6363f81ec170c916d1ed4
Noah Moroze <noahm@moroze.com>
2013-07-07 04:56:57 +0000

    Changed homepage to read contents of index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send("I'm watching you 0.0");
+  var html = fs.readFileSync("index.html");
+  response.send(html.toString('utf8')); //converts buffer to string
 });
 
 app.get('/hi',function(request, response) {
